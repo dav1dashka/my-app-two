76196639a65b823c2b34f34ca653c40bee77ac9b
dav1dashka <dujadavyd17@gmail.com>
2024-01-15 20:38:20 +0200

    Update fetch-data.component.ts

--- src/app/fetch-data/fetch-data.component.ts
+++ src/app/fetch-data/fetch-data.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Output, inject, EventEmitter } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-// import { GetWeatherComponent } from '../get-weather/get-weather.component';
 
 @Component({
   selector: 'app-fetch-data',
@@ -18,12 +17,10 @@ export class FetchDataComponent {
 
   ngOnInit(): void {
     this.fetchData();
-    console.log(1)
-
   }
 
   fetchData() {
-    this.httpClient.get('https://randomuser.me/api/')
+    this.httpClient.get('https://randomuser.me/api/?results=3')
       .subscribe((data) => {
         this.userdata = data;
         this.setCardUserData();
@@ -31,32 +28,10 @@ export class FetchDataComponent {
   }
 
   setCardUserData() {
-    this.cardUserData =  this.userdata.results;
-    console.log(this.cardUserData)
-    // this.userdata.results.forEach((element: any, key: number) => {
-    //   this.cardUserData[`user${key}`] = {};
-    //   this.cardUserData[`user${key}`]['image'] = element.picture.medium;
-    //   this.cardUserData[`user${key}`]['name'] = element.name;
-    //   this.cardUserData[`user${key}`]['location'] = element.location;
-    //   this.cardUserData[`user${key}`]['gender'] = element.gender;
-    //   this.cardUserData[`user${key}`]['email'] = element.email;
-    // });
-
+    this.cardUserData = this.userdata.results
+console.log(this.cardUserData)
     this.isLoaded = true;
   }
 
-  // setCardData(data: any) {
 
-  //   if (this.isSent) {
-  //     this.weatherData.forEach((element: any, key: number) => {
-  //       this.cardUserData[`user${key}`][`weather`] = {};
-  //       this.cardUserData[`user${key}`][`weather`]['image'] = element.image;
-  //       this.cardUserData[`user${key}`][`weather`]['description'] = element.description;
-  //       this.cardUserData[`user${key}`]['weather']['currentTemp'] = element.currentTemp;
-  //       this.cardUserData[`user${key}`]['weather']['lowestTemp'] = element.lowestTemp;
-  //       this.cardUserData[`user${key}`]['weather']['higestTemp'] = element.higestTemp;
-  //     });
-  //   }
-
-  // }
-}
+  }
